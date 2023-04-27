967c3e6e9ba9b46319647807ab2d2a6698bb6715
Haasith Venkata Sai <haasith.pasala@research.iiit.ac.in>
2023-04-27 13:50:45 +0530

    done

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import torch
 from spinup import EpochLogger
 from spinup.utils.logx import restore_tf_graph
+import gym
 
 
 def load_policy_and_env(fpath, itr='last', deterministic=False):
@@ -115,9 +116,15 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
         "page on Experiment Outputs for how to handle this situation."
 
     logger = EpochLogger()
+    if "BulletEnv" in env.spec.id:
+        env = gym.make(env.spec.id)
+        if render:
+            # pybullet envs have to call env.render before env.reset
+            env.render()
     o, r, d, ep_ret, ep_len, n = env.reset(), 0, False, 0, 0, 0
     while n < num_episodes:
         if render:
+
             env.render()
             time.sleep(1e-3)
 
